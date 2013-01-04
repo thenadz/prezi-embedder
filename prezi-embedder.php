42c18712930cc6dc3605d960b3f2cec1568e51f7
thenadz <dan.rossiters@gmail.com>
2013-01-04 01:12:03 +0000

    Tagging 1.1.3: Fixes bug with lock_to_path attribute.
    git-svn-id: http://plugins.svn.wordpress.org/prezi-embedder/tags/1.1.3@647671 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ prezi-embedder.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Plugin Name: Prezi Embedder
+Plugin URI: http://wordpress.org/extend/plugins/prezi-embedder/
+Description: Allows for embedding the newest iframe-based Prezis from <a href="prezi.com/recommend/qv1ms7qvtplw">prezi.com</a> using a simple shortcode [prezi id="&lt;your id here&gt;"].
+Version: 1.1.3
+Author: Dan Rossiter
+Author URI: http://danrossiter.org/
+*/
+
+  function prezi_shortcode( $atts ){
+    extract( shortcode_atts( array(
+      'id' => null,
+      'width' => 500,
+      'height' => 400,
+      'lock_to_path' => 0
+    ), $atts ) );
+
+    $err = '';
+
+    // VALIDATE INPUT
+    if(!is_numeric( $width ))
+      $err .= "Error: width attribute must be numeric. You entered width=$width. ";
+    if(!is_numeric( $height ))
+      $err .= "Error: height attribute must be numeric. You entered height=$height. ";
+    if($lock_to_path != 0 && $lock_to_path != 1)
+      $err .= "Error: lock_to_path may only be 0 or 1. You entered lock_to_path=$lock_to_path. ";
+    if(!$id)
+      $err .= "Error: You must, at minimum include an id attribute: [prezi id='&lt;Prezi ID&gt;'] ";
+    // END VALIDATION
+
+    if($err){
+      return $err;
+    } else {
+      $ptn = '#.*prezi.com/([^/]+).*#';
+      $rpl = "$1";
+      $id = preg_replace( $ptn, $rpl, $id );
+      return '<!-- Prezi Embedder -->'.PHP_EOL.
+             "<iframe src='http://prezi.com/embed/{$id}/?bgcolor=ffffff&amp;lock_to_path={$lock_to_path}&amp;autoplay=no&amp;autohide_ctrls=0' ".
+             "width='{$width}' height='{$height}' frameBorder='0'></iframe>".PHP_EOL.
+             '<!-- / Prezi Embedder -->'.PHP_EOL;
+    }
+  }
+  add_shortcode( 'prezi', 'prezi_shortcode' );
+
+?>
