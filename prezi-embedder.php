96794600c8f514c5869090a6983fc8b754eb38b0
thenadz <dan.rossiters@gmail.com>
2014-04-01 03:26:59 +0000

    Tagging v1.3 * New embed code * I18n * Everything moved to PreziEmbedder class
    git-svn-id: http://plugins.svn.wordpress.org/prezi-embedder/trunk@885441 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- prezi-embedder.php
+++ prezi-embedder.php
@@ -18,21 +18,23 @@ add_shortcode('prezi', array('PreziEmbedder', 'doShortcode'));
 // I18n
 add_action('plugins_loaded', array('PreziEmbedder', 'loadTextDomain'));
 
+PreziEmbedder::init();
+
 class PreziEmbedder {
-   private $bool_err, $comment, $int_err, $id_err, $min_req;
+   private static $attr_err, $bool_err, $comment, $int_err, $min_req;
    
-   public function __construct() {
-      $this->comment =
+   public static function init() {
+      self::$comment =
          '<!-- ' .
          __('Generated using Prezi Embedder. Get yours here:', 'prezi-embedder') .
          ' http://wordpress.org/plugins/prezi-embedder/ -->' . PHP_EOL;
-      $this->bool_err =
-         __('Error: The %1$s attribute may only be %2$s or %3$s. You entered %1$s=%4$s', 'prezi-embedder');
-      $this->int_err = 
-         __('Error: The %1$s attribute must be an integer. You entered %1$s=%2$s. ', 'prezi-embedder');
-      $this->attr_err  =
+      self::$attr_err  =
          __('Error: The %1$s attribute provided does not look right. You entered %1$s=%2$s. ', 'prezi-embedder');
-      $this->min_req =
+      self::$bool_err =
+         __('Error: The %1$s attribute may only be %2$s or %3$s. You entered %1$s=%4$s', 'prezi-embedder');
+      self::$int_err = 
+         __('Error: The %1$s attribute must be an integer. You entered %1$s=%2$s. ', 'prezi-embedder');
+      self::$min_req =
          __('Error: You must, at minimum include an id attribute:', 'prezi-embedder');
    }
    
@@ -42,6 +44,8 @@ class PreziEmbedder {
     * @return string The embed code on success or error string(s) on failure.
     */
    public static function doShortcode($atts) {
+      static $ptn = '#.*prezi.com/([^/]+).*#';
+      
       extract(shortcode_atts(
          array(
              'id' => null,
@@ -50,28 +54,27 @@ class PreziEmbedder {
              'lock_to_path' => 0),
          $atts));
 
-      $ptn = '#.*prezi.com/([^/]+).*#';
       $err = '';
 
       // VALIDATE INPUT
       if (!($id = preg_replace($ptn, '$1', $id))) {
-         $err .= sprintf($this->attr_err, 'id', $id);
+         $err .= sprintf(self::$attr_err, 'id', $id);
       }
       
       if ((int) $width != $width || (int) $width < 1) {
-         $err .= sprintf($this->int_err, 'width', $width);
+         $err .= sprintf(self::$int_err, 'width', $width);
       }
       
       if ((int) $height != $height || (int) $height < 1) {
-         $err .= sprintf($this->int_err, 'height', $height);
+         $err .= sprintf(self::$int_err, 'height', $height);
       }
       
       if ($lock_to_path != 0 && $lock_to_path != 1) {
-         $err .= sprintf($this->bool_err, 'lock_to_path', 0, 1, $lock_to_path);
+         $err .= sprintf(self::$bool_err, 'lock_to_path', 0, 1, $lock_to_path);
       }
       
       if (!$id) {
-         $err .= $this->min_req . ' [prezi id=\'&lt;Prezi ID&gt;\'] ';
+         $err .= self::$min_req . ' [prezi id=\'&lt;Prezi ID&gt;\'] ';
       }
       // END VALIDATION
 
@@ -79,6 +82,7 @@ class PreziEmbedder {
       if ($err) return $err;
       
       return
+         self::$comment .
          "<iframe src='http://prezi.com/embed/{$id}/?bgcolor=ffffff&amp;" .
          "lock_to_path={$lock_to_path}&amp;autoplay=0&amp;autohide_ctrls=0&amp;" .
          "features=undefined&amp;disabled_features=undefined' " .
